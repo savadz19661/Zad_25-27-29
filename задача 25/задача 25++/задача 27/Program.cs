ac339842424acffa2569da4c3b497bb12157a6a8
Slava Kirin <savadz1966@gmail.com>
2022-12-09 12:12:09 +0300

    задача 27

---
+++ задача 25/задача 25++/задача 27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.Write ("Введите число: ");
+int limit = int.Parse(Console.ReadLine ());
+
+Console.WriteLine ($"Сумма цифр числа {limit} равна {GetSum(limit)}");
+int GetSum (int limit)
+{
+    int sum = 0;
+    while ( limit > 0)
+    {
+         sum += limit % 10; limit /= 10;
+    } 
+
+    return sum;
+}
